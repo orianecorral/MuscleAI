c4f38abd2e7cbf939c8100d5ec73c8d24f527f02
Oriane <oriane.corral@epitech.eu>
2025-02-13 16:10:57 +0100

    creation of small database

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'muscleai_db',
+        'USER': 'muscleai_user',
+        'PASSWORD': 'muscleai_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
